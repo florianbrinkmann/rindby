851b006f479e3d80381917c87cf80838cac3cc49
Florian Brinkmann <mail@flobri.de>
2016-08-14 17:54:07 +0200

    „Hannover“ durch „Rindby“ in Autoren-Hinweis ersetzt

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer id="footer">
 	<?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
-	<p class="theme-author"><?php _e( 'Theme: Hannover by <a rel="nofollow" href="https://florianbrinkmann.de">Florian Brinkmann</a>', 'rindby' ) ?></p>
+	<p class="theme-author"><?php _e( 'Theme: Rindby by <a rel="nofollow" href="https://florianbrinkmann.de">Florian Brinkmann</a>', 'rindby' ) ?></p>
 </footer>
 </div>
 <?php wp_footer(); ?>
